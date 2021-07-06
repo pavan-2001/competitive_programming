8c2877d9cf2f243b8b865e9bfbfa44d94e515dae
pavan-2001 <apavank71@gmail.com>
2021-07-06 14:01:45 +0530

    https://www.codingninjas.com/codestudio/problems/letter-case-permutation_1232630

--- Backtracking/Letter Case Permutation/main.cpp
+++ Backtracking/Letter Case Permutation/main.cpp
@@ -4,37 +4,34 @@ using namespace std;
 #define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define endl '\n'
 
-string temp;
 
-void myFunction(vector<string> &result, string &s, int start) {
-    if(temp.size() == s.size()) {
-        //cout<<temp;
-        result.push_back(temp);
+
+void myFunction(vector<string> &result, string &s, int i) {
+    if(i == s.size() ){
+        result.push_back(s);
         return ;
     }
-    for(int i=start;i<s.size();i++) {
-        temp.push_back(tolower(s[i]));
-        myFunction(result, s, i+1);
-        temp.pop_back();
-        temp.push_back(toupper(s[i]));
-        myFunction(result, s, i+1);
-        temp.pop_back();
-    }
+   
+     if(s[i] >= '0' && s[i] <= '9') {
+            myFunction(result, s, i+1);
+        } else {
+            s[i] = toupper(s[i]);
+            myFunction(result, s, i+1);
+            s[i] = tolower(s[i]);
+            myFunction(result, s, i+1);
+        }
 }
 
 vector<string> letCasePermutation(string s) {
 	// Write your code here
-    vector<string> result;
+   	vector<string> result; 
     myFunction(result, s, 0);
-    sort(result.begin(), result.end());
-    auto it = unique(result.begin(), result.end());
-    result.erase(it, result.end());
-    return result;
+   return result;
 }
 
 int main() {
     fast
-    //cout << "\x1b[2J\x1b[1;1H"<<endl;
+    cout << "\x1b[2J\x1b[1;1H"<<endl;
     int t;
     cin>>t;
     while(t--) {
