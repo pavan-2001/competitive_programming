ae67520dc809e2b923946c4d6fed8e5ae4f5bca7
pavan-2001 <apavank71@gmail.com>
2021-07-08 14:33:36 +0530

    Competitive Programming

---
+++ Sorting And Searching/Bubble Sort/main.cpp
@@ -0,0 +1,37 @@
+/*
+    Time Complexity : O(N^2).
+    N : Size of array.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+void countingSort(vector<int> &arr, int &n) {
+    for(int i=0;i<n;i++) {
+        for(int j=i+1;j<n;j++) {
+            if(arr[i] > arr[j]) 
+            swap(arr[i], arr[j]);
+        }
+    }
+}
+
+int main() {
+    fast   
+
+    cout << "\033[2J\033[1;1H";
+
+    int n = 8;
+
+    vector<int> arr = {1, 3, 2, 8, 2, 5, 6, 9};
+
+    countingSort(arr, n); 
+
+    for(auto i : arr)
+    cout<<i<<' ';
+
+    return 0;
+}
+
